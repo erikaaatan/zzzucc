5116c8451431b07376abd0cab92001201687c658
PalashTanejaPro <tanejapalash@gmail.com>
2018-08-14 22:22:35 +0530

    added better pattern matching for good bois

--- index.js
+++ index.js
@@ -3,6 +3,30 @@ const BootBot = require('bootbot');
 const config = require('config');
 var fetch = require("node-fetch");
 
+function sendReminder(){
+
+}
+
+function createReminder(){
+
+}
+
+function sendGoodBoyes(userID){
+    bot.say(userID, 'Searching for the perfect gif...');
+  fetch(GIPHY_URL + "Puppies")
+    .then(res => res.json())
+    .then(json => {
+      bot.say(
+        userID, 
+      {
+        attachment: 'image',
+        url: json.data.image_url
+      }, {
+        typing: true
+      });
+    });
+}
+
 const bot = new BootBot({
     accessToken: config.get('access_token'),
     verifyToken: config.get('verify_token'),
@@ -22,17 +46,15 @@ const GIPHY_URL = `http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag
 
 
 bot.hear(['cute', 'dog', '(pic)?tures', 'doggo'], (payload, chat) => {
-chat.say('Searching for the perfect gif...');
-  fetch(GIPHY_URL + "Puppies")
-    .then(res => res.json())
-    .then(json => {
-      chat.say({
-        attachment: 'image',
-        url: json.data.image_url
-      }, {
-        typing: true
-      });
-    });
+
 });
+
+bot.on('message', (payload, chat) => {
+    const text = payload.message.text;
+    if (text.includes("dog") || text.includes("puppy") || text.includes("puppies") || text.includes("pic") || text.includes("pictures")|| text.includes("pics") || text.includes("boys") || text.includes("good")) {
+        sendGoodBoyes(payload.sender.id);
+    }
+});
+
 
 bot.start();

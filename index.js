8b9c844b2c1d4d0f09464481ec64a8a0e0f606e9
erikaaatan <eatan18@gmail.com>
2018-08-14 12:41:12 -0400

    basic tip feature

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const BootBot = require('bootbot');
 const config = require('config');
 var fetch = require("node-fetch");
 
+var tips = [
+  "Sleep polyphasically: divide your sleep into smaller blocks per day instead of in one big block. This will reduce the amount of sleep you need per day and help you wake up feeling more alert.",
+  "Keep your hands and feet warm at night-wear socks!",
+  "People tend to sleep better in cooler settings. Set your room temperature to 60-67 degrees Fahrenheit or 15-19 degrees Celsius.",
+  "Exercise regularly, but do it at least a couple hours before bedtime so you can cool down.",
+  "Resist the habit of thinking in bed and clear your mind before bedtime.",
+  "Take a hot bath before you go to bed tonight. It won't just help you fall asleep faster, but it'll also help you get higher quality sleep too!",
+  "Get out of bed if you can't sleep-otherwise, your body will be accustomed to doing nothing while awake in bed.",
+  "Try using another pillow tonight!"
+];
+
 const bot = new BootBot({
     accessToken: config.get('access_token'),
     verifyToken: config.get('verify_token'),
@@ -16,6 +27,10 @@ bot.on('message', (payload, chat) => {
 
 bot.hear(['hello', 'hi', 'hey', 'oi'], (payload, chat) => {
     chat.say("Hello again!");
+});
+
+bot.hear(['tip'], (payload, chat) => {
+    chat.say(tips[Math.floor(Math.random()*tips.length)]);
 });
 
 const GIPHY_URL = `http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=`;
